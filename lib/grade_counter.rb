cba34aed488e24db87db6f2e7ae121bfc3c7c2e3
Connor Worthington <worthington.connor@gmail.com>
2020-10-13 09:26:31 +0100

    refactors testing for counting green grades

--- lib/grade_counter.rb
+++ lib/grade_counter.rb
@@ -1,8 +1,7 @@
 class Grade_Counter
 
   def count(grades)
-   # return 'Green: 1'
-   green_count = 0 
+    green_count = 0 
     grades_array = grades.split(",")
     grades_array.each do |grade|
       if grade == "Green"
--- spec/grade_counter_spec.rb
+++ spec/grade_counter_spec.rb
@@ -2,13 +2,13 @@ require 'Grade_Counter'
 
 RSpec.describe Grade_Counter do 
 
+  let(:grade_counter) { Grade_Counter.new }
+
   describe '#grade_count' do 
     it 'returns Green: 1 when given Green ' do 
-      grade_counter = Grade_Counter.new
       expect(grade_counter.count('Green')).to eq('Green: 1')
     end
     it 'returns Green: 2 when given "Green, Green"' do 
-      grade_counter = Grade_Counter.new 
       expect(grade_counter.count('Green,Green')).to eq("Green: 2") 
     end 
   end 
