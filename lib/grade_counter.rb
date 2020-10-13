d0ad844b9fde0084378b4b3d576d5524148c82f1
Connor Worthington <worthington.connor@gmail.com>
2020-10-13 09:25:16 +0100

    Adding conditionality to .count
    .count will now sum up number of green grades

--- lib/grade_counter.rb
+++ lib/grade_counter.rb
@@ -1,7 +1,15 @@
 class Grade_Counter
 
   def count(grades)
-    return 'Green: 1'
+   # return 'Green: 1'
+   green_count = 0 
+    grades_array = grades.split(",")
+    grades_array.each do |grade|
+      if grade == "Green"
+        green_count += 1
+      end 
+    end 
+    return "Green: #{green_count}"
   end 
 
 end 
