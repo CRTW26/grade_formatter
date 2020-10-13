4c2bbda29f68d0051253bdff8f30519e8c501d72
Connor Worthington <worthington.connor@gmail.com>
2020-10-13 09:44:44 +0100

    Refactors iterating through array in .count

--- lib/grade_counter.rb
+++ lib/grade_counter.rb
@@ -7,13 +7,9 @@ class Grade_Counter
 
     grades_array = grades.split(",")
     grades_array.each do |grade|
-      if grade == "Green"
-        green_count += 1
-      elsif grade == "Amber"
-        amber_count += 1
-      elsif grade == 'Red'
-        red_count += 1
-      end 
+      if grade == "Green" then green_count += 1 end 
+      if grade == "Amber" then amber_count += 1 end
+      if grade == 'Red' then red_count += 1 end 
     end 
 
     if (green_count && amber_count && red_count) > 0 
@@ -22,7 +18,8 @@ class Grade_Counter
       return "Green: #{green_count}\n Amber: #{amber_count}"
     else 
       return "Green: #{green_count}"
-    end 
+    end
+
   end 
 
 end 
