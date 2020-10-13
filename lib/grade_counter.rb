fbffc3512f7f9ae31a93256946b1df19a412f010
Connor Worthington <worthington.connor@gmail.com>
2020-10-13 09:41:12 +0100

    Adds red grades to .count method

--- lib/grade_counter.rb
+++ lib/grade_counter.rb
@@ -3,15 +3,22 @@ class Grade_Counter
   def count(grades)
     green_count = 0 
     amber_count = 0 
+    red_count = 0 
+
     grades_array = grades.split(",")
     grades_array.each do |grade|
       if grade == "Green"
         green_count += 1
       elsif grade == "Amber"
         amber_count += 1
+      elsif grade == 'Red'
+        red_count += 1
       end 
     end 
-    if (green_count && amber_count) > 0 
+
+    if (green_count && amber_count && red_count) > 0 
+      return "Green: #{green_count}\n Amber: #{amber_count}\n Red: #{red_count}"
+    elsif (green_count && amber_count) > 0
       return "Green: #{green_count}\n Amber: #{amber_count}"
     else 
       return "Green: #{green_count}"
--- spec/grade_counter_spec.rb
+++ spec/grade_counter_spec.rb
@@ -14,6 +14,9 @@ RSpec.describe Grade_Counter do
     it 'returns Green: 2, Amber: 1 when give "Green, Green, Amber"' do 
       expect(grade_counter.count('Green,Green,Amber')).to eq("Green: 2\n Amber: 1")
     end 
+    it 'returns Green:1, Amber: 1, Red: 1 when given "Green, Amber, Red"' do 
+      expect(grade_counter.count('Green,Amber,Red')).to eq("Green: 1\n Amber: 1\n Red: 1")
+    end 
   end 
 
 end 
